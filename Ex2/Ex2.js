070c1a9f26c33a67150c1e740e437f23d53f94e3
Ruoyu Li <r883l717@ku.edu>
2020-03-02 15:01:38 -0600

    Ex2 add

--- Ex2/Ex2.js
+++ Ex2/Ex2.js
@@ -1,16 +1,10 @@
-function Ex2()
+function Ex2p()
 {
-  var p1 = document.getElementById("p1").value;
-  var p2 = document.getElementById("p2").value;
-  if (p1 == p2) {
-    if(p1.length<8) {
-      alert("The passwords are not at least 8 characters long");
-    }
-    else {
-      alert("Pass the validator");
-    }
-  }
-  else {
-    alert("The passwords entered don't match");
-  }
+  var var = document.getElementById("myImgId").value;
+  alert(var);
+}
+
+function Ex2p()
+{
+  
 }
